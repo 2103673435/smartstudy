d534e10ed69a2788a9289b39b9670fa36f60d08a
thebowenfeng <857514.leofeng@gmail.com>
2021-08-20 19:07:48 +1000

    initial commit

---
+++ src/backend/resource_server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///auth.db"
+CORS(app)
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run(port=5002)
