d70ad7125a227598d4977f93c7f063fe75bac628
thebowenfeng <857514.leofeng@gmail.com>
2021-08-20 19:25:26 +1000

    setup listnere server

--- src/desktop/Smart Study Desktop/WindowsFormsApp1/Form1.Designer.cs
+++ src/desktop/Smart Study Desktop/WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(257, 167);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(215, 87);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 438);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLogin;
     }
 }
 
--- src/desktop/Smart Study Desktop/WindowsFormsApp1/Form1.cs
+++ src/desktop/Smart Study Desktop/WindowsFormsApp1/Form1.cs
@@ -1,20 +1,65 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SimpleHttp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var cancel = new CancellationTokenSource();
+
+            Route.Add("/{Query}", async (req, res, props) =>
+            {
+                res.AsText("You can safely close the page.");
+                cancel.Cancel();
+
+                var payload = new Dictionary<string, string>
+                {
+                    { "ClientID", "react" },
+                    { "ClientSecret", "my secret" },
+                    { "AuthCode", req.QueryString.Get("auth_code")}
+                };
+
+                var jsonContent = JsonConvert.SerializeObject(payload);
+                var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                HttpClient client = new HttpClient();
+                var response = await client.PostAsync("http://127.0.0.1:5001/get_access_token", httpContent);
+
+                Dictionary<string, string> tokens = JsonConvert.DeserializeObject<Dictionary<string, string>>(await response.Content.ReadAsStringAsync());
+
+                MessageBox.Show(tokens["access_token"]);
+            });
+
+            HttpServer.ListenAsync(
+                1337,
+                cancel.Token,
+                Route.OnHttpRequestAsync
+                );
+
+            System.Diagnostics.Process.Start("http://127.0.0.1:5001/login?callback=http://localhost:1337");
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
